3463f1e5e42154bea13b2aa7a781bad19f5ec2c5
Andrea <greenlounge@hotmail.com>
2019-07-05 15:05:35 -0500

    Node.js errors: Manejo de errores en Node.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const error = require('./src/errors');
+const handle = require ('./src/handling');
+
+handle.errorFirstCallback();
+
+
+/* 
+
+Tipo de errores:
+
+Standar Javascript errors
+Errores de sintaxis, de evaluación, contrucción de URIs, etc.
+
+System errors
+Cuando se falla en la lectura de algún archivo, etc.
+
+User-specified errors
+Cuando lanzamos un Throw y especificamos un mensaje personalizado.
+
+Assertion Errors
+Errores que definen aspectos lógicos dentro de Node.js. Violaciones de lógica como TRUE == FALSE, por ejemplo
+
+*/
